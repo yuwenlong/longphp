24408e12746cc4913c2c9897d375687c59bb1543
yuwenlong <yuwenlong@wenlong.org>
2016-01-05 20:40:38 +0800

    升级smarty

--- conf/smarty.conf.php
+++ conf/smarty.conf.php
@@ -3,7 +3,7 @@ if(!defined('DIR')){
 	exit('Please correct access URL.');
 }
 
-require_once DIR.'class/smarty/Smarty.class.php';
+require_once DIR_CLASS.'smarty/Smarty.class.php';
 $smarty = new Smarty();
 $smarty->template_dir = DIR_TPL;
 $smarty->compile_dir = DIR.'tpl_c/';
